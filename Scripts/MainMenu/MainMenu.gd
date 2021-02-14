3519c0f8cb04cf67c9861ff29d2926d52dda1da1
rednir <davran.dilshat@gmail.com>
2021-02-14 14:30:31 +0000

    add main menu animation

--- Scripts/MainMenu/MainMenu.gd
+++ Scripts/MainMenu/MainMenu.gd
@@ -2,12 +2,13 @@ extends Control
 
 
 const GAME_NAME = "Drawing Game"
-const GAME_VERSION = "0.3"
+const GAME_VERSION = "0.4"
 
 
 onready var text_game_info = $TextGameInfo
 onready var textbox_join = $MainButtonsContainer/JoinContainer/TextboxJoin
 onready var textbox_username = $MainButtonsContainer/TextboxUsername
+onready var main_buttons_animation_player = $MainButtonsContainer/MainButtonsAnimation
 
 
 const min_name_length = 2
@@ -18,35 +19,59 @@ func _ready():
 	$MainButtonsContainer/ButtonCreate.connect("pressed", self, "on_button_create_pressed")
 
 	text_game_info.bbcode_text = "%s v%s" % [GAME_NAME, GAME_VERSION]
+	main_buttons_animation_player.play("in")
+
+
 
 
 
 
 func on_button_join_pressed():
+	disable_buttons(true)
+	main_buttons_animation_player.play("out")
+	yield(main_buttons_animation_player, "animation_finished")
+
 	var status_code = Client.try_join_server(textbox_join.text)
 	if len(textbox_username.text) < min_name_length:
 		Events.emit_signal("error", "Username is too short")
-		return
-
-	if status_code != OK:
-		Events.emit_signal("error", "Could not join the server (Code: %s)\nMake sure the IP address exists." % status_code)
 	else:
-		Client.this_player.name = textbox_username.text
-		get_tree().change_scene("res://Scenes/Game.tscn")
+		if status_code != OK:
+			Events.emit_signal("error", "Could not join the server (Code: %s)\nMake sure the IP address exists." % status_code)
+		else:
+			Client.this_player.name = textbox_username.text
+			get_tree().change_scene("res://Scenes/Game.tscn")
+			return
+
+	disable_buttons(false)
+	main_buttons_animation_player.play("in")
 
 
 
 		
 func on_button_create_pressed():
+	disable_buttons(true)
+	main_buttons_animation_player.play("out")
+	yield(main_buttons_animation_player, "animation_finished")
+
 	var status_code = Server.try_create_server()
 	if len(textbox_username.text) < min_name_length:
 		Events.emit_signal("error", "Username is too short")
-		return
-
-	if status_code != OK:
-		Events.emit_signal("error", "Could not create a server (Code: %s)" % status_code)
-		Server.server = null
 	else:
-		Client.this_player.name = textbox_username.text
-		Client.try_join_server("localhost")
-		get_tree().change_scene("res://Scenes/Game.tscn")
+		if status_code != OK:
+			Events.emit_signal("error", "Could not create a server (Code: %s)" % status_code)
+			Server.server = null
+		else:
+			Client.this_player.name = textbox_username.text
+			Client.try_join_server("localhost")
+			get_tree().change_scene("res://Scenes/Game.tscn")
+			return
+		
+	disable_buttons(false)
+	main_buttons_animation_player.play("in")
+
+
+
+
+func disable_buttons(will_disable):
+	$MainButtonsContainer/JoinContainer/ButtonJoin.disabled = will_disable
+	$MainButtonsContainer/ButtonCreate.disabled = will_disable
