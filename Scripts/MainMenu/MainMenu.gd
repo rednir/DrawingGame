c2db43a45c25b81bd09d5f96ace9851d2924476f
rednir <davran.dilshat@gmail.com>
2021-02-18 17:03:32 +0000

    add exit button

--- Scripts/MainMenu/MainMenu.gd
+++ Scripts/MainMenu/MainMenu.gd
@@ -5,7 +5,7 @@ const SettingsScene = preload("res://Scenes/SettingsMenu.tscn")
 const LoadingIconScene = preload("res://Scenes/LoadingIcon.tscn")
 
 
-onready var text_game_info = $TextGameInfo
+onready var text_game_info = $TopBarContainer/TextGameInfo
 onready var textbox_join = $MainButtonsContainer/JoinContainer/TextboxJoin
 onready var textbox_username = $MainButtonsContainer/TextboxUsername
 onready var main_buttons_animation_player = $MainButtonsContainer/MainButtonsAnimation
@@ -19,12 +19,13 @@ var settings_instance
 func _ready():
 	VisualServer.set_default_clear_color(Color("2c2c2f"))
 
-	$BottomButtonsContainer.visible = false
+	$BottomBarContainer.visible = false
 
 	$MainButtonsContainer/JoinContainer/ButtonJoin.connect("pressed", self, "on_button_join_pressed")
 	$MainButtonsContainer/ButtonCreate.connect("pressed", self, "on_button_create_pressed")
-	$BottomButtonsContainer/ButtonSettings.connect("pressed", self, "on_button_settings_pressed")
-	$BottomButtonsContainer/ButtonGithub.connect("pressed", self, "on_button_github_pressed")
+	$BottomBarContainer/ButtonSettings.connect("pressed", self, "on_button_settings_pressed")
+	$BottomBarContainer/ButtonGithub.connect("pressed", self, "on_button_github_pressed")
+	$TopBarContainer/ButtonExit.connect("pressed", self, "on_button_exit_pressed")
 	
 	if Server.public_ip == null:
 		Server.ip_http_req.connect("request_completed", self, "on_ip_got")
@@ -32,7 +33,7 @@ func _ready():
 	else:
 		on_ip_got(null, null, null, null)
 
-	text_game_info.bbcode_text = "%s %s" % [Settings.GAME_NAME, Settings.GAME_VERSION]
+	text_game_info.bbcode_text = "%s %s\nTODO: ADD SETTINGS MENU (volume, resolution, fullscreen, default name)" % [Settings.GAME_NAME, Settings.GAME_VERSION]
 
 
 
@@ -42,7 +43,7 @@ func on_ip_got(_result, _response_code, _headers, _body):
 	main_buttons_animation_player.play("in")
 	if loading_icon_instance != null:		# might cause crashes in release build
 		loading_icon_instance.get_node("AnimationPlayer").play("out")
-	$BottomButtonsContainer.visible = true
+	$BottomBarContainer.visible = true
 
 
 
@@ -90,7 +91,7 @@ func on_button_create_pressed():
 
 
 func on_button_settings_pressed():
-	$BottomButtonsContainer.visible = false
+	$BottomBarContainer.visible = false
 	main_buttons_transition("out")
 	yield(main_buttons_animation_player, "animation_finished")
 
@@ -102,7 +103,7 @@ func on_button_settings_pressed():
 
 
 func on_button_settings_back_pressed():
-	$BottomButtonsContainer.visible = true
+	$BottomBarContainer.visible = true
 	main_buttons_transition("in")
 	settings_instance.queue_free()
 
@@ -113,6 +114,14 @@ func on_button_github_pressed():
 	var code = OS.shell_open("https://github.com/%s" % Settings.REPO_PATH)
 	if code != OK:
 		Events.emit_signal("error", "Could not open page.")
+
+
+
+
+func on_button_exit_pressed():
+	$TopBarContainer/ButtonExit.disabled = true
+	get_tree().quit()
+
 
 
 
